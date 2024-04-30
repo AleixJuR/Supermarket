ea594110e4acc4333d7a7b2e500c7dead93847e6
AleixJuR <154283119+AleixJuR@users.noreply.github.com>
2024-04-30 17:25:35 +0200

    prova2

---
+++ Supermarket/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    public abstract class Person
+    {
+        protected string _id;
+        protected string _fullName;
+        protected int _points;
+        private bool active = false;
+        protected Person(string id, string fullName, int points) 
+        {
+            _id = id;
+            _fullName = fullName;
+            _points = points;
+        }
+        protected Person(string id, string fullName) :this(id,fullName,0) { }
+        public string fullName
+        {
+            get => _fullName;
+        }
+
+    }
+}
